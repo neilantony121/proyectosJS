94ea83bb5e3490c273d50b6fc0b8656bd47bdf13
neilantony <antonyloky121@gmail.com>
2023-06-10 22:25:25 -0400

    primer commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const colors = {
+    Rojo: "#FF0000",
+    Verde: "#00FF00",
+    Azul: "#0000FF",
+    Amarillo: "#FFFF00",
+    Cian: "#00FFFF",
+    Magenta: "#FF00FF",
+    Blanco: "#FFFFFF",
+    Negro: "#000000",
+    Gris: "#808080",
+    Marrón: "#A52A2A",
+    Naranja: "#FFA500",
+    Rosa: "#FFC0CB",
+    Violeta: "#EE82EE",
+    Verdeclaro: "#90EE90",
+    Azulclaro: "#ADD8E6",
+    Grisclaro: "#D3D3D3"
+    };
+const buttonChangeColor = document.querySelector('#buttonChangeColor');
+
+
+
+// funcion para cambiar de color
+function changeColor(){
+    
+}
+
+
+buttonChangeColor.addEventListener('click',changeColor);
